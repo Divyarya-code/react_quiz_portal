235f12d6807ed40e74c6eb74005c0d6af79dd4e6
Divyarya-code <71401691+Divyarya-code@users.noreply.github.com>
2022-02-16 16:03:36 +0530

    my first commit reactwebsite

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div>
+            Made with Love by{" "}
+            <a href="https://github.com/Divyarya-code" style={{cursor: "pointer"}}>
+                Divyarya
+            </a>
+        </div>
+    );
+}
+export default Footer;
